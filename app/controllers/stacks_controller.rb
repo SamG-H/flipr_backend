e4fa990c5131322185b298863768726834272299
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-29 12:52:47 -0800

    add body to stacks controller

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -1,2 +1,36 @@
 class StacksController < ApplicationController
+  before_action :set_stack, only: [:show, :update, :destroy]
+  
+  def index
+    @stacks = Stack.all
+    json_response(@stacks)
+  end
+
+  def show
+    json_response(@stack)
+  end
+
+  def create
+    @stack = Stack.create!(stack_params)
+    json_response(@stack, :created)
+  end
+
+  def update
+    @stack.update(stack_params)
+    head :no_content
+  end
+
+  def destroy
+    @stack.destroy
+    head :no_content
+  end
+
+  private
+  def stack_params
+    params.permit(:title)
+  end
+
+  def set_stack
+    @stack = Stack.find(params[:id])
+  end
 end
