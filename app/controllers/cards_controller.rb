1559c6002af54d5b7409ceaaa70ffad2b5c5671c
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-12-02 12:09:51 -0800

    add rest methods to cards controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,46 @@
 class CardsController < ApplicationController
+  before_action :set_stack
+  before_action :set_stack_card, only: [:show, :update, :destroy]
+
+  # GET /stacks/:stack_id/cards
+  def index
+    json_response(@stack.cards)
+  end
+
+  # GET /stacks/:stack_id/cards/:id
+  def show
+    json_response(@card)
+  end
+
+  # POST /stacks/:stack_id/cards
+  def create
+    @stack.cards.create!(card_params)
+    json_response(@stack, :created)
+  end
+
+  # PUT /stacks/:stack_id/cards/:id
+  def update
+    @card.update(card_params)
+    head :no_content
+  end
+
+  # DELETE /stacks/:stack_id/cards/:id
+  def destroy
+    @card.destroy
+    head :no_content
+  end
+
+  private
+
+  def card_params
+    params.permit(:front, :back)
+  end
+
+  def set_stack
+    @stack = Stack.find(params[:stack_id])
+  end
+
+  def set_stack_card
+    @card = @stack.cards.find_by!(id: params[:id]) if @stack
+  end
 end
